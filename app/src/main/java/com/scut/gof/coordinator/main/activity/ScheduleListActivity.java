043310f37ca85267f1fa4d18e30001001fe91137
newpasung <975033141@qq.com>
2015-12-22 14:04:59 +0800

    为了pullcommit一下

---
+++ app/src/main/java/com/scut/gof/coordinator/main/activity/ScheduleListActivity.java
@@ -0,0 +1,11 @@
+package com.scut.gof.coordinator.main.activity;
+
+import com.scut.gof.coordinator.main.activity.base.BaseActivity;
+
+/**
+ * Created by Administrator on 2015/12/22.
+ */
+public class ScheduleListActivity extends BaseActivity {
+
+
+}
